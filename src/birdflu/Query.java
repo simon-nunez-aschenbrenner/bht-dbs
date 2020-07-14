03876c89b980b08c9bdb63b1d1e570c1c92dd74f
simonaschenbrenner <mail@simonaschenbrenner.com>
2020-07-15 01:32:01 +0200

    Gen Line Chart Impl

--- src/birdflu/Query.java
+++ src/birdflu/Query.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.logging.Logger;
 
@@ -30,8 +31,10 @@ public class Query {
 		}
 	}
 	
-	public int[] getIntArray(int column) throws NoDataException {
+	public LinkedList<Integer> getList(int column) throws NoDataException {
+		
 		LinkedList<Integer> list = new LinkedList<Integer>();
+		
 		try {
 			while (result.next()) {
 				list.add(result.getInt(column));
@@ -41,13 +44,37 @@ public class Query {
 			e.getSQLState();
 			e.getErrorCode();
 		}
-		if (list.size() < 1) { throw new NoDataException(query); }
-		int[] array = new int[list.size()];
-		for (int i = 0; i < array.length; i++) {
-			array[i] = list.get(i).intValue();
+		
+		if (list.size() < 1) {
+			throw new NoDataException(query);
+		} else {
+//			for(Integer i : list) {
+//				System.out.println(i);
+//			}
+			return list;
 		}
-		return array;
-	}	
+	}
+	
+	public HashMap<String, Integer> getMap(int keyColumn, int valueColumn) throws NoDataException {
+		
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		
+		try {
+			while (result.next()) {
+				map.put(result.getString(keyColumn), result.getInt(valueColumn));
+			}
+		} catch (SQLException e) {
+			e.getMessage();
+			e.getSQLState();
+			e.getErrorCode();
+		}
+		
+		if (map.size() < 1) {
+			throw new NoDataException(query);
+		} else {
+			return map;
+		}
+	}
 	
 	public boolean close() {
 		try {
